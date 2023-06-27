e0bbeaef41324192e7b180862b46ecd068a52419
Ahmed Awad <awad55397@gmail.com>
2023-06-27 13:47:58 +0200

    adding verification function in JS

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+function submit(){
+  var input= prompt("please verify that you are a human: whats the sum of -1 + 20");
+  if(input==19){
+    alert("thank you for yout time");
+  }else{
+    alert("you entered invalid input");
+  }
+}
